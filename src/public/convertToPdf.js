02a0aea1d48e0030a450d86f30942e14f93fdeb8
Luis Evilla <luisevilla588@gmail.com>
2020-12-11 05:16:03 -0500

    generar pdf del pqr

--- src/public/convertToPdf.js
+++ src/public/convertToPdf.js
@@ -1,33 +1,10 @@
-const bcrypt = require('bcryptjs');
-
-const helpers = {}
-
-helpers.encrypPassword = async (password) => {
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-    return hash;
-}
-
-helpers.mathPassword = async (password, savedPassword) => {
-    try {
-        return await bcrypt.compare(password, savedPassword);
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-helpers.convertToPdf = () => {
-    const html2pdf = require('html2pdf.js');
-
-
 const $boton = document.querySelector('#pdfBtn');
 $boton.addEventListener('click', () => {
-    console.log("click");
     const $elementoParaConvertir = document.querySelector('#pqr')
     html2pdf()
     .set({
         margin: 1,
-        filename: 'documento.pdf',
+        filename: 'pqr.pdf',
         image: {
             type: 'jpeg',
             quality: 0.98
@@ -46,6 +23,4 @@ $boton.addEventListener('click', () => {
     .save()
     .catch(err => console.log(err));
 })
-}
 
-module.exports = helpers;
