d1e83c15cc45f18a2e4ea541500701dded5a596b
Luis Evilla <luisevilla588@gmail.com>
2021-08-16 20:15:46 -0500

    Modificando configuraicon de base de datos

--- src/config.js
+++ src/config.js
@@ -7,7 +7,13 @@ module.exports = {
         host: process.env.DB_HOST || 'localhost',
         database: process.env.DB_NAME || 'pqrs',
         password: process.env.DB_PASSWORD || 'password',
-        port: process.env.DB_PORT || 5432
+        port: process.env.DB_PORT || 5432,
+        ssl: true,
+        extra: {
+            ssl: {
+                rejectUnauthorized: false
+            },
+        },
     },
     SECRET: process.env.SECRET || 'secret'
 }
